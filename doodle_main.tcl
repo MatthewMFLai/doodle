27125278d6815e0c61ce20f5f2ac7c860a883cc1
AL1945 <42256544+AL1945@users.noreply.github.com>
2018-08-09 21:35:00 -0400

    Register IP Address
    Register IP Address

--- doodle_main.tcl
+++ doodle_main.tcl
@@ -2,5 +2,5 @@ source doodle.tcl
 source vm_client.tcl
 
 VM_Client::Init
-set handle [VM_Client::Register localhost 14000 doodle'handle_remote]
+set handle [VM_Client::Register 192.168.2.244 14000 doodle'handle_remote]
 doodle 400 400 $handle
